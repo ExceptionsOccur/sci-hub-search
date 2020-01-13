cc6ee7a9f9547ec5be0295d5ec20452e2b9dd87e
Onyx <Onyx@DESKTOP-8BJGGT6>
2020-01-13 15:18:24 +0800

    v0.0.1

--- background.js
+++ background.js
@@ -13,15 +13,43 @@ browser.search.get().then(createMenuItem);
 
 browser.menus.onClicked.addListener((info, tab) => {
 		var httpRequest = new XMLHttpRequest();
-		httpRequest.open('GET', 'https://api.crossref.org/works?query=' + info.selectionText, true);
-		httpRequest.send();
-		httpRequest.onreadystatechange = function () {
-		if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-			var text = httpRequest.responseText;
-			var json = JSON.parse(text);
+		var selected = text_proc(info.selectionText);
+		if(selected[0] == '1'){
 			browser.tabs.create({
-				url: 'http://sci-hub.tw/' + json.message.items[0].DOI,
-    		});
+				url: 'http://sci-hub.tw/' + selected,
+			});
 		}
-	};
+		else{
+			httpRequest.open('GET', 'https://api.crossref.org/works?query.bibliographic=' + selected, true);
+			httpRequest.send();
+			httpRequest.onreadystatechange = function () {
+				if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+					var text = httpRequest.responseText;
+					var json = JSON.parse(text);
+					if(json.message.items.length == 0 || selected != json.message.items[0].title[0]){
+						browser.notifications.create({
+    						"type": "basic",
+    						"title": "search by sci-hub",
+    						"message": selected + '\n' + "its DOI is not found"
+    					});
+					}
+					else
+						browser.tabs.create({
+							url: 'http://sci-hub.tw/' + json.message.items[0].DOI,
+			    		});
+				}
+			};
+		}
 });
+
+
+
+function text_proc(text) {
+	if(text[0] == ' '){
+		text = text.substr(1,text.length);
+	}
+	if(text[text.length - 1] == ' '){
+		text = text.substr(0,text.length - 1);
+	}
+	return text.replace(/\s+/g,' ');
+}
