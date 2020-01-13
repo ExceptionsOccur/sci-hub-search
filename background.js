124acb7146504efee9f06773ee55051e473c550b
Onyx <Onyx@DESKTOP-8BJGGT6>
2020-01-13 11:09:40 +0800

    search by title, TODO: delay is too long.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 
-
-function createMenuItem(engines) {
+var doi = '';
+function createMenuItem() {
     browser.menus.create({
       id: 'search',
       title: 'search by sci-hub',
@@ -12,7 +12,16 @@ browser.search.get().then(createMenuItem);
 
 
 browser.menus.onClicked.addListener((info, tab) => {
-  browser.tabs.create({
-            url: 'http://sci-hub.tw/' + info.selectionText,
-          });
+		var httpRequest = new XMLHttpRequest();
+		httpRequest.open('GET', 'https://api.crossref.org/works?query=' + info.selectionText, true);
+		httpRequest.send();
+		httpRequest.onreadystatechange = function () {
+		if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+			var text = httpRequest.responseText;
+			var json = JSON.parse(text);
+			browser.tabs.create({
+				url: 'http://sci-hub.tw/' + json.message.items[0].DOI,
+    		});
+		}
+	};
 });
