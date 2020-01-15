c8bbd41d0810b20975c886b68821344902fba4f5
Onyx <Onyx@DESKTOP-8BJGGT6>
2020-01-15 10:29:36 +0800

    add url in title and doi, fixed bugs.

--- content-script.js
+++ content-script.js
@@ -7,26 +7,27 @@ show_page();
 const SCI_HUB = "http://sci-hub.tw/";
 
 function show_page() {
-var nnnname = localStorage.getItem('name');
-var httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', 'https://api.crossref.org/works?query.author='+ nnnname + '&rows=50', true);
-httpRequest.send();
-httpRequest.onreadystatechange = function () {
-	if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-		var text = httpRequest.responseText;
-		var json = JSON.parse(text);
-		if(json.message.items.length == 0){
-			document.getElementsByClassName('index')[0].getAttributeNode("style").value="display:";
-			document.getElementsByClassName('top_author')[0].innerHTML = nnnname + " any journals were found";
+	var nnnname = localStorage.getItem('name');
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.open('GET', 'https://api.crossref.org/works?query.author='+ nnnname + '&rows=50', true);
+	httpRequest.send();
+	httpRequest.onreadystatechange = function () {
+		if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+			var text = httpRequest.responseText;
+			var json = JSON.parse(text);
+			if(json.message.items.length == 0){
+				document.getElementsByClassName('index')[0].getAttributeNode("style").value="display:";
+				document.getElementsByClassName('top_author')[0].innerHTML = nnnname + " any journals were found";
+			}
+			else{
+				updata_by_data(json);
+				document.getElementsByClassName('index')[0].getAttributeNode("style").value="display:";
+				document.getElementsByClassName('top_author')[0].innerHTML = nnnname;
+				document.getElementsByClassName('foot')[0].getAttributeNode("style").value="display:";
+			}
 		}
-		else{
-			document.getElementsByClassName('index')[0].getAttributeNode("style").value="display:";
-			document.getElementsByClassName('top_author')[0].innerHTML = nnnname;
-			updata_by_data(json);
-		}
+		localStorage.clear();					
 	}
-	localStorage.clear();					
-}
 }
 
 
@@ -34,59 +35,78 @@ httpRequest.onreadystatechange = function () {
 
 // input author
 function authors_proc(author) {
-	var all_author = author[0].given + ' ' + author[0].family;
-	var temp = "";
-	if (author.length > 1) {
-		for (var i = 1; i < author.length; i++) {
-			if (i == 3) {
-				all_author = all_author.concat(' et al.');
-				return all_author;
-			}
-			else{
-				temp = ', ' + author[i].given + ' ' + author[i].family;
-				all_author = all_author.concat(temp);
+	if(author[0]["given"] != null){
+		var all_author = author[0].given + ' ' + author[0].family;
+		var temp = "";
+		if (author.length > 1) {
+			for (var i = 1; i < author.length; i++) {
+				if (i == 3) {
+					all_author = all_author.concat(' et al.');
+					return all_author;
+				}
+				else{
+					temp = ', ' + author[i].given + ' ' + author[i].family;
+					all_author = all_author.concat(temp);
+				}
 			}
 		}
+		return all_author;
 	}
-	return all_author;
+	else{
+		var all_author = author[0].family;
+		var temp = "";
+		if (author.length > 1) {
+			for (var i = 1; i < author.length; i++) {
+				if (i == 3) {
+					all_author = all_author.concat(' et al.');
+					return all_author;
+				}
+				else{
+					temp = ', ' + ' ' + author[i].family;
+					all_author = all_author.concat(temp);
+				}
+			}
+		}
+		return all_author;
+	}
 }
-
-// function ISSN_proc(ISSN) {
-// 	var issn = ISSN[0];
-// 	if(ISSN.length > 1){
-// 		for (var i = 1; i < ISSN.length; i++) {
-// 			issn = issn.concat('/' + ISSN[i]);
-// 		}
-// 	}
-// 	return issn;
-// }
 
 
 function add_node_by_data(data) {
-	var title = data.title[0];
+	var title = "";
 	var author = authors_proc(data.author);
 	var doi = data.DOI;
-	var references_count = data["reference-count"];
-	var journal = data["container-title"][0];
+	var link = "";
+	var journals = "";
 	var year = data.created["date-parts"][0][0];
 	var doc_url = SCI_HUB + doi;
-	// var issue = data.issue;
-	// var volume = data.Volume;
-	// var issn = ISSN_proc(data.ISSN);
-	// var page = data.page;
+	if(data.link == null){
+		link = data.URL;
+	}
+	else{
+		link = data.link[0].URL;
+	}
+	if(data["container-title"] == null){
+		journals = data.publisher;
+	}
+	else{
+		journals = data["container-title"][0];
+	}
+	if(data.title == null){
+		title = "null";
+	}else{
+		title = data.title[0];
+	}
 	href_attr = document.getElementsByClassName('title')[0];
-	href_attr.getAttributeNode("href").value=doc_url;
+	href_attr.getAttributeNode("href").value = doc_url;
+	link_attr = document.getElementsByClassName('link')[0];
+	link_attr.getAttributeNode("href").value = link;
 	var li = document.getElementsByClassName('result_list')[0].cloneNode(true);
 	li.getElementsByClassName('title')[0].innerHTML = title;
 	li.getElementsByClassName('author')[0].innerHTML = author;
 	li.getElementsByClassName('doi')[0].innerHTML = doi;
-	li.getElementsByClassName('reference_count').innerHTML = references_count;
-	li.getElementsByClassName('journal')[0].innerHTML = journal;
+	li.getElementsByClassName('journals')[0].innerHTML = journals;
 	li.getElementsByClassName("year")[0].innerHTML = year;
-	// li.getElementsByClassName('issue')[0].innerHTML = issue;
-	// li.getElementsByClassName('volume')[0].innerHTML = volume;
-	// 	li.getElementsByClassName('issn')[0].innerHTML = issn;
-	// li.getElementsByClassName('page')[0].innerHTML = page;
 	li.getAttributeNode("style").value="display:";
 	document.getElementsByClassName('cap')[0].appendChild(li);
 }
